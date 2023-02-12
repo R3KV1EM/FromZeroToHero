dec1d27ff8bc0dce0db710a9cd00db47165629fa
Archy <smok3rarchy@gmail.com>
2023-02-13 03:41:46 +0700

    tye

--- 4EPH.py
+++ 4EPH.py
@@ -1,3 +1,37 @@
-peremen
-trebuyut
-nashi serdca
+import random
+
+table = list(range(37))
+x = list(range(1, 19))
+z = list(range(19, 37))
+
+print("Welcome to Roulette, place a bet")
+bet = input("Place your bet on 1-18 (input 't') or 19-36 (input 'y') : ")
+
+spin = random.choice(table)
+print(f"The ball landed on: {spin}")
+
+if bet == "t" and spin in x:
+    print("Congratulations! You win.")
+elif bet == "y" and spin in z:
+    print("Congratulations! You win.")
+else:
+    print("Sorry, better luck next time.")
+
+    import random
+
+    table = list(range(37))
+    bet_range_1 = list(range(1, 19))
+    bet_range_2 = list(range(19, 37))
+
+    print("Welcome to Roulette, place a bet")
+    bet = input("Place your bet on 1-18 (input 't') or 19-36 (input 'y') : ")
+
+    spin = random.choice(table)
+    print(f"The ball landed on: {spin}")
+
+    if bet == "t" and spin in bet_range_1:
+        print("Congratulations! You win.")
+    elif bet == "y" and spin in bet_range_2:
+        print("Congratulations! You win.")
+    else:
+        print("Sorry, better luck next time.")
--- ROULLET NEED SOLVE ASAP.py
+++ ROULLET NEED SOLVE ASAP.py
@@ -1,10 +1,9 @@
 import random
-
 print("Hello Welcome to Roullete")
-roulettable = tuple(range(0,37))
+roulettable = tuple(range(0, 37))
 balance = 1000
-st1 = list(range(1,18))
-st2 = list(range(19,36))
+st1 = list(range(1, 18))
+st2 = list(range(19, 36))
 print(roulettable)
 print("If you want to place 1-18 write X or 19-36 write Z")
 bet = input("Place a bet, from 0 to 36, ")
