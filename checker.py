c37edc6e715462537034fad9ccfab1b5e418ecda
Archy <smok3rarchy@gmail.com>
2023-02-13 14:25:37 +0700

    new

--- checker.py
+++ checker.py
@@ -1,2 +1,14 @@
-Тут еще ибо 
-
+# Penalty Shootout
+import random
+def penalty():
+    shoot = [True, False]
+    bam = random.choice(shoot)
+    if bam == True:
+        print("Goal scored")
+    else:
+        print("Miss")
+good = []
+bad = []
+while penalty() <30:
+    if bam == True:
+        good.append()
